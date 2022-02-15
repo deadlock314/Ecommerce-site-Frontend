0bb4bbaa39400465b107767074b151e3d4cc117f
chandan soni <chandansoni2270@gmail.com>
2022-02-15 12:56:07 +0530

    added spinner in cart and modified contexts to store prevcart data

--- src/CartComponents/Cart.js
+++ src/CartComponents/Cart.js
@@ -2,24 +2,29 @@ import React, { useContext, useEffect, useState } from 'react';
 import EmptyCart from './EmptyCart';
 import CartWithProduct from './CartWithProduct';
 import { useParams } from 'react-router'; 
-import {getProductInfoData} from '../HelperFun/getProductData';
-import { CartContext,ContextArr} from '../HelperFun/Context';
+import {getProductListData} from '../HelperFun/getProductData';
+import {ContextArr} from '../HelperFun/Context';
+import {priceAdder} from '../HelperFun/priceAdder';
+import Spinner from '../unitComponent/Spinner';
 
 function Cart() {
 
 const param=useParams();
 const Contextarr=useContext(ContextArr);
-const [cartProduct,setCartProduct]=useState();
+const [cartProduct,setCartProduct]=useState(Contextarr[0].prevCartData);
 const [loading ,setLoading]=useState(true);
-
 
 
 useEffect(()=>
 {
      const FetchData=async()=>{
-    const res= await getProductInfoData(param,Contextarr[0].productType)
-    setCartProduct([res])
-   
+    const res= await getProductListData(`https://ecommerce-app-api-1.herokuapp.com/singleproduct/${Contextarr[0].productType}/${param.ProductId}`)
+    setCartProduct([res]);
+
+    Contextarr[1]({...Contextarr[0],priceObj : priceAdder(Contextarr[0].priceObj,res.price,'add'),
+       prevCartData : Contextarr[0].prevCartData.concat([res]) ,
+      countObj : { ...Contextarr[0].countObj,[res._id]:1}});
+
     setLoading(false)
 
 }
@@ -28,15 +33,12 @@ useEffect(()=>
 },[]);
 
 
-
-
-
                 return (
-                  <CartContext>
+                  <>
                   {
-                     loading ?  <> </>  : cartProduct.length <=0 ? <EmptyCart/>: <CartWithProduct props={cartProduct} />
+                     loading ?  <Spinner/>  : cartProduct.length <=0 ? <EmptyCart/>: <CartWithProduct props={cartProduct.price} />
                   }
-                  </CartContext>
+                  </>
                   )
 
 
